1db88ab3986d08ea7f25e59841a9f56513658cd8
Sean Everest <sean.b.everest@gmail.com>
2015-04-13 23:15:54 -0700

    message detail html view

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,4 @@
-var zabox = angular.module('zabox', ['ngRoute', 'zaboxServices', 'ngAnimate']);
+var zabox = angular.module('zabox', ['ngRoute', 'zaboxServices', 'ngAnimate', 'ngSanitize']);
 var zaboxServices = angular.module('zaboxServices', ['ngResource']);
 
 zabox.config(['$routeProvider',
