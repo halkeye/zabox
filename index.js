868c93f780a843879d27ef4a0d8c1a862e96d068
Gavin Mogan <gavin@kodekoan.com>
2015-04-13 19:33:43 -0700

    some basic endpoints

--- index.js
+++ index.js
@@ -8,7 +8,19 @@ var port = process.env.PORT || 3000;
 app.use(serveStatic(__dirname + '/public'));
 
 app.get('/api', function(req, res) {
-  res.json(['endpoint1','endpoint2']);
+  res.json(['json']);
+});
+
+app.get('/api/json', function(req, res) {
+  res.json(['messages']);
+});
+
+app.get('/api/json/messages', function(req, res) {
+  res.json([{id: "guid-goes-here", from: "Gavin Mogan <gavin@gavinmogan.com>", subject: "This is my first message"}]);
+});
+
+app.get('/api/json/messages/:guid', function(req, res) {
+  res.json({id: "guid-goes-here", from: "Gavin Mogan <gavin@gavinmogan.com>", subject: "This is my first message"});
 });
 
 app.listen(port, function() {
