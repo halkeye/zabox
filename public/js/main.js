207940b3775dad001aa9cbd1d9c1c3434bbfa39b
Sean Everest <sean.b.everest@gmail.com>
2015-04-14 23:54:38 -0700

    momentjs for timestamps

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,4 @@
-var zabox = angular.module('zabox', ['ngRoute', 'zaboxServices', 'ngAnimate', 'ngSanitize']);
+var zabox = angular.module('zabox', ['ngRoute', 'zaboxServices', 'ngAnimate', 'ngSanitize', 'angularMoment']);
 var zaboxServices = angular.module('zaboxServices', ['ngResource']);
 
 zabox.config(['$routeProvider',
