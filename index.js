42925b6d15a6e9f3d453d4d66c1f9f00737a5832
Gavin Mogan <gavin@kodekoan.com>
2015-04-14 23:19:33 -0700

    make index now run the smtp server

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ var port = process.env.PORT || 3000;
 var storage = new Storage();
 
 var WebServer = require('./lib/webserver');
+var SmtpServer = require('./lib/smtp');
+
+
 var webserver = new WebServer(port, storage);
 webserver.run();
 
+var smtpServer = new SmtpServer(port+1, null, storage);
+smtpServer.run();
--- smtp_server.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-var Storage = require('./lib/storage/test.js');
-var storage = new Storage()
-require('./lib/smtp.js')(3001, null, storage);
