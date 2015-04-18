7f98e106bbbde61aba0bcc0f1ce29b1f9a86e0cd
Gavin Mogan <gavin@gavinmogan.com>
2015-04-18 10:15:49 -0700

    as per gulp-boilerplate, make gulp karma actually error out properly so we get nice exit codes

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var debug = require('gulp-debug');
 var jasmine = require('gulp-jasmine');
 var karma = require('gulp-karma');
+var plumber = require('gulp-plumber');
 
 var paths = {
 	//scripts: ['client/js/**/*.coffee', '!client/external/**/*.coffee'],
@@ -28,15 +29,12 @@ gulp.task('karma', function() {
   // listed in karma.conf.js INSTEAD of what was passed to
   // gulp.src !
   return gulp.src('./foobar')
+    .pipe(plumber())
     .pipe(karma({
       configFile: 'karma.conf.js',
       action: 'run'
     }))
-    .on('error', function(err) {
-      // Make sure failed tests cause gulp to exit non-zero
-      console.log(err);
-      this.emit('end'); //instead of erroring the stream, end it
-    });
+    .on('error', function(err) { throw err; });
 });
 
 gulp.task('test', ['jasmine'], function() {});
