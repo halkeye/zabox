13f89ea4532f35f8e3afacc05b9f1d8875dbc7ff
Gavin Mogan <gavin@kodekoan.com>
2015-04-17 21:49:46 -0700

    command line parameters to change the port and such

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 'use strict';
-var Storage = require('./lib/storage/test.js');
-var port = process.env.PORT || 3000;
-var storage = new Storage();
+var program = require('commander');
 
+program
+  .version(require(__dirname + '/package.json').version)
+  .description(require(__dirname + '/package.json').description)
+  .option('-w, --webport <n>', 'Web Port', parseInt)
+  .option('-s, --smtpport <n>', 'SMTP Port', parseInt)
+  .option('--storage <storage>', 'Storage Mode [test,memory,sqlite]', /^(test|memory|sqlite)$/, 'memory')
+  .parse(process.argv);
+
+
+var Storage = require('./lib/storage/' + program.storage + '.js')
+var storage = new Storage();
 var WebServer = require('./lib/webserver');
 var SmtpServer = require('./lib/smtp');
 
+var webPort = program.webport || 3000;
+var smtpPort = program.smtpport || webPort + 1;
 
-var webserver = new WebServer(port, storage);
+var webserver = new WebServer(webPort, storage);
 webserver.run();
 
-var smtpServer = new SmtpServer(port+1, null, storage);
+var smtpServer = new SmtpServer(smtpPort , null, storage);
 smtpServer.run();
